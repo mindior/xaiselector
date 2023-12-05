6c7a234663df7d041b61b0d17bf8afa7d85fb7cf
Marcelo Indio dos Reis <mindior@users.noreply.github.com>
2023-12-05 17:22:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -68,8 +68,8 @@ def create_app():
 
     return app
 
-#if __name__ == '__main__':
-#    app = create_app()
-#    with app.app_context():
-#        db.create_all()
-#        app.run(debug=True)
+if __name__ == '__main__':
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
